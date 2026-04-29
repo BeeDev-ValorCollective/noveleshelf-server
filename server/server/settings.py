3a6700daba1342553f882a4a5616b37ce8f65c96
melissa-24 <mlongenberger@codingdojo.com>
2026-04-29 11:14:16 -0400

    updated refreshtokens

--- server/server/settings.py
+++ server/server/settings.py
@@ -61,6 +61,8 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
 }
 CRONJOBS = [
     ('0 0 * * *', 'cron.user_cron.deactivate_unverified_users'),
