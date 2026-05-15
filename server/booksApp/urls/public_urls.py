d0464c84e99bb0c63d3396cd2a707540e096ebc6
melissa-24 <mlongenberger@codingdojo.com>
2026-05-14 20:05:32 -0400

    added ref data endpoint

--- server/booksApp/urls/public_urls.py
+++ server/booksApp/urls/public_urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('featured/', public_views.featured),
     path('books/', public_views.book_list),
     path('books/<int:book_id>/', public_views.book_detail),
+    path('books/reference-data/', public_views.book_reference_data, name='book-reference-data'),
 ]
