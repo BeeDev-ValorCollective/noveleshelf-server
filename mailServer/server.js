08328e4bed41b4d56c4a96095e74a879b5335167
melissa-24 <mlongenberger@codingdojo.com>
2026-06-01 14:36:46 -0400

    saving

--- mailServer/server.js
+++ mailServer/server.js
@@ -4,17 +4,18 @@ const cookieParser = require("cookie-parser")
 const app = express()
 require("dotenv").config()
 
-app.use(
-    cookieParser(),
-    express.json(),
-    express.urlencoded({ extended: true }),
-    cors({
-        credentials: true,
-        origin: process.env.NODE_ENV === 'development'
-            ? process.env.FRONTEND_DEV_ORIGIN
-            : process.env.FRONTEND_PROD_ORIGIN
-    })
-)
+app.use(cors({
+    credentials: true,
+    origin: [
+    process.env.FRONTEND_DEV_ORIGIN,
+    process.env.FRONTEND_PROD_ORIGIN,
+    process.env.FRONTEND_PREVIEW_ORIGIN
+].filter(Boolean)
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const mailRoutes = require("./routes/mail.routes")
 app.use('/api', mailRoutes)
