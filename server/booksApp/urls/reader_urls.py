b304a20d208e86a5fd1108c2d14af2869d59ba61
melissa-24 <mlongenberger@codingdojo.com>
2026-06-18 20:03:51 -0400

    updated to proper url path

--- server/booksApp/urls/reader_urls.py
+++ server/booksApp/urls/reader_urls.py
@@ -6,10 +6,10 @@ from booksApp.views.reader_views import (
 
 urlpatterns = [
     # Reader - Library
-    path('reader/library/', MyLibraryView.as_view(), name='my-library'),
-    path('reader/library/<int:book_id>/', MyLibraryBookView.as_view(), name='my-library-book'),
+    path('library/', MyLibraryView.as_view(), name='my-library'),
+    path('library/<int:book_id>/', MyLibraryBookView.as_view(), name='my-library-book'),
 
     # Reader - Chapters
-    path('reader/chapters/<int:chapter_id>/read/', ChapterReadView.as_view(), name='chapter-read'),
-    path('reader/chapters/<int:chapter_id>/unlock/', ChapterUnlockView.as_view(), name='chapter-unlock'),
+    path('chapters/<int:chapter_id>/read/', ChapterReadView.as_view(), name='chapter-read'),
+    path('chapters/<int:chapter_id>/unlock/', ChapterUnlockView.as_view(), name='chapter-unlock'),
 ]
