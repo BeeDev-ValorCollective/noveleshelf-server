78bc6440aa2b6f84004d8d4217797f5376f0ffcf
melissa-24 <mlongenberger@codingdojo.com>
2026-07-02 16:04:41 -0400

    updating to add validations to usernames and added new route

--- server/userApp/urls/public_urls.py
+++ server/userApp/urls/public_urls.py
@@ -3,4 +3,5 @@ from userApp.views import public_views
 
 urlpatterns = [
     path('authors/', public_views.public_authors),
+    path('authors/<str:username>/', public_views.public_author_detail),
 ]
